4f1242a2fff10d1224132974652153224f8c337b
Kevin Day <kevin.r.day@gmail.com>
2015-09-06 19:04:45 -0400

    cleaning things up a bit

--- 7.rb
+++ 7.rb
@@ -3,18 +3,17 @@ primes = [2,3]
 nth = 10001
 
 for i in (2..nth) do
-  j=1
-  num = primes[i-1]+2
+  j = 1
+  num = primes[i - 1]+2
   while (j<i) do
-    if num%primes[j]==0 then
-      num+=2
-      j=1
+    if num%primes[j] == 0 then
+      num += 2
+      j = 1
       next
     end
     j = j + 1
   end
-  primes[i]=num
+  primes[i] = num
 end
 
-
-puts primes[nth-1]
+puts primes[nth - 1]
