2c65f5e4f047dc55ed0abba6f052f079cdf1e904
Kevin Day <kevin.r.day@gmail.com>
2015-09-13 22:40:31 -0500

    Solution to 27 now runs in .314s vs 3.5

--- 27.rb
+++ 27.rb
@@ -3,12 +3,13 @@ require_relative "helpers"
 maxA = 999
 maxB = 999
 
+@sieve = getSieve(100000)
+
 def consecutivePrimes(a, b)
     n = 0
     while true
       result = n ** 2 + a * n + b
-      #puts result
-      break if !prime?(result)
+      break if @sieve[result] == 0
       n += 1
     end
     n
@@ -17,7 +18,9 @@ end
 maxPrimes = 0
 product = nil
 (maxA * -1).upto(maxA) do |a|
-  (maxB * -1).upto(maxB) do |b|
+  1.upto(maxB) do |b|
+    #b has to be prime, because n's go away at n=0
+    next if @sieve[b] == 0
     numPrimes = consecutivePrimes(a, b)
     if numPrimes > maxPrimes then
       maxPrimes = numPrimes
