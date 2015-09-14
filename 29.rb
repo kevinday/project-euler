988b469b1cdf5b1de1180478f08920e3996336ce
Kevin Day <kevin.r.day@gmail.com>
2015-09-13 23:15:33 -0500

    very small perfomance gain using a set

--- 29.rb
+++ 29.rb
@@ -1,12 +1,14 @@
+require 'set'
 maxA = 100
 maxB = 100
 
-results = []
+results = Set.new
 
 2.upto(maxA) do |a|
   2.upto(maxB) do |b|
-    results.push(a ** b)
+    results.add(a ** b)
   end
 end
 
-puts results.sort.uniq.length
+
+puts results.to_set.length
